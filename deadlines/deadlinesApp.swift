cef444b899da02b6850a6463879e80a3871831c6
Saurish Srivastava <saurishs1215@gmail.com>
2025-06-13 15:22:27 -0700

    Initial Commit

---
+++ deadlines/deadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  deadlinesApp.swift
+//  deadlines
+//
+//  Created by saurish on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct deadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
